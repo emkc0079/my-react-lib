7acd60f794b756c53bd96e01bdf6a6cece5922cc
Ahmad Hamdan <ahamdan@emkanfinco.com.sa>
2025-01-01 11:56:38 +0300

    sd

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,14 @@
-const path = require("path");
-
 module.exports = {
-  entry: "./src/index.tsx", // Update this to your actual entry point
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/, // This matches both .ts and .tsx files
+        test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"], // Add file extensions to resolve
+    extensions: [".tsx", ".ts", ".js"],
   },
-  mode: "development", // or 'production'
 };
